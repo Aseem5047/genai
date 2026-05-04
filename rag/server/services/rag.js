d3bc40f34b5962d6fe1d9d53d48444d865a79bcc
Aseem5047 <aseemgupta2002@gmail.com>
2026-05-04 11:02:27 +0530

    updated changes

--- rag/server/services/rag.js
+++ rag/server/services/rag.js
@@ -3,5 +3,7 @@ import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 export async function indexTheDocument(path) {
     const loader = new PDFLoader(path)
     const docs = await loader.load()
+
+    console.log(docs);
 }
 
